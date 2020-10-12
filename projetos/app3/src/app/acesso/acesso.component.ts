384dfdcd3a8ab95fbf3207a2576ca0eed612b9d4
raulsantiago <raulsantiago@id.uff.br>
2020-10-12 18:26:51 -0300

    Feito até aula 259 - Cria app3 Insta clone

---
+++ projetos/app3/src/app/acesso/acesso.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+@Component({
+  selector: 'app-acesso',
+  templateUrl: './acesso.component.html',
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px, 0)' }),
+        animate('500ms 1s ease-in-out') // duração, delay e eceleração
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0)' }),
+        // Entendendo os Keyframes em %
+        // 0 void -----x------------------------x---x---x------------x criado 1500ms
+        animate('1500ms 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 1, opacity: 1, transform: 'translateY(0)' })
+        ])) // duração, delay e eceleração
+      ])
+    ])
+  ]
+})
+export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
+
+  public cadastro: boolean = false
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public exibirPainel(event: string): void {
+    this.cadastro = event === 'cadastro' ? true : false
+  }
+
+  public inicioDaAnimacao(): void {
+    console.log('inicio animanção')
+  }
+
+  public fimDaAnimacao(): void {
+    console.log('fim animanção')
+  }
+
+}
